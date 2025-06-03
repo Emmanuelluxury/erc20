5cee2d0625d2ebdf0e82468b4b8cc25b9e7da230
Emmanuelluxury <emmanuelokanandu99@gmail.com>
2025-06-03 16:11:36 +0100

    update code

--- src/Vault.sol
+++ src/Vault.sol
@@ -8,16 +8,20 @@ contract Vault {
     address public owner;
     uint256 public totalFees;
 
+    constructor(address tokenAddress) {
+        token = ERC20(tokenAddress);
+        owner = msg.sender;
+    }
+
     mapping(address => uint256) private balances;
+    uint256 private _totalDeposits;
+    uint256 private _accumulatedFees;
 
     event Deposited(address indexed user, uint256 amount);
     event Withdrawn(address indexed user, uint256 amount, uint256 fee);
     event FeesWithdrawn(address indexed owner, uint256 amount);
-
-    constructor(address _token) {
-        token = ERC20(_token);
-        owner = msg.sender;
-    }
+    event FeeUpdated(uint256 oldFee, uint256 newFee);
+    event TotalDepositsUpdated(uint256 newTotalDeposits);
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Not the contract owner");
@@ -26,31 +30,33 @@ contract Vault {
 
     function deposit(uint256 amount) external {
         require(amount > 0, "Deposit amount must be greater than zero");
-        token.transferFrom(msg.sender, address(this), amount);
+        require(token.transferFrom(msg.sender, address(this), amount), "Transfer failed");
         balances[msg.sender] += amount;
         emit Deposited(msg.sender, amount);
     }
 
     function withdraw(uint256 amount) external {
+        require(amount > 0, "Withdraw amount must be greater than zero");
         require(balances[msg.sender] >= amount, "Insufficient balance");
         uint256 fee = (amount * 5) / 100; // 5% fee
-        uint256 withdrawAmount = amount - fee;
+        uint256 net = amount - fee;
 
         balances[msg.sender] -= amount;
         totalFees += fee;
+        require(token.transfer(msg.sender, net), "Token transfer failed");
 
-        token.transfer(msg.sender, withdrawAmount);
-        emit Withdrawn(msg.sender, withdrawAmount, fee);
+        emit Withdrawn(msg.sender, net, fee);
     }
 
     function withdrawFees() external onlyOwner {
         require(totalFees > 0, "No fees to withdraw");
-        uint256 feesToWithdraw = totalFees;
+        uint256 amount = totalFees;
         totalFees = 0;
-        token.transfer(owner, feesToWithdraw);
-        emit FeesWithdrawn(owner, feesToWithdraw);
+        require(token.transfer(owner, amount), "Fee transfer failed");
+        emit FeesWithdrawn(owner, amount);
     }
 
+    // Getter functions
     function getBalance(address user) external view returns (uint256) {
         return balances[user];
     }
--- test/Vault.t.sol
+++ test/Vault.t.sol
@@ -1,3 +1,66 @@
+
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.26;
 
+import "forge-std/Test.sol";
+import "../src/Vault.sol";
+import "../src/ERC20.sol";
+
+contract VaultTest is Test {
+    Vault  vault;
+    ERC20 token;
+    address user = address(1);
+    address owner;
+
+    function setUp() public {
+        owner = address(this);
+        token = new ERC20("Test Token", "TST", 18, 1_000_000 ether);
+        vault = new Vault(address(token));
+
+        // Give user some tokens and approve the vault
+        token.transfer(user, 1000 ether);
+        vm.prank(user);
+        token.approve(address(vault), 1000 ether);
+    }
+
+    function testDeposit() public {
+        vm.prank(user);
+        vault.deposit(100 ether);
+        assertEq(vault.getBalance(user), 100 ether);
+    }
+
+    function testWithdrawWithFee() public {
+        vm.prank(user);
+        vault.deposit(200 ether);
+
+        vm.prank(user);
+        vault.withdraw(100 ether);
+
+        // 5% of 100 = 5 fee, user should get 95
+        assertEq(token.balanceOf(user), 1000 ether - 200 ether + 95 ether);
+        assertEq(vault.getBalance(user), 100 ether); // 200 - 100 withdrawn
+        assertEq(vault.getTotalFees(), 5 ether);
+    }
+
+    function testWithdrawFees() public {
+        vm.prank(user);
+        vault.deposit(200 ether);
+
+        vm.prank(user);
+        vault.withdraw(100 ether);
+
+        vault.withdrawFees();
+        assertEq(token.balanceOf(owner), 1_000_000 ether - 1000 ether + 5 ether);
+    }
+
+    function testOnlyOwnerCanWithdrawFees() public {
+        vm.prank(user);
+        vault.deposit(100 ether);
+        vm.prank(user);
+        vault.withdraw(100 ether);
+
+        vm.prank(user);
+        vm.expectRevert("Not the contract owner");
+        vault.withdrawFees();
+    }
+}
